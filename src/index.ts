222840ee1576917f562d6d7109f8f5969fb0528c
김현하(visual) <118410962+jaranda-visual@users.noreply.github.com>
2024-05-06 16:10:01 +0900

    initialize

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 console.log('initialize')
+
+type Office = {
+  name: string;
+  address: Address;
+}
+type Address = {
+  street: string;
+  city: string;
+  state: string;
+  postalCode: string;
+}
+type User = {
+  name: string;
+  address: Address[];
+  office: Office;
+}
+
+
+
